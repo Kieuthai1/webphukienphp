d1ede73af5ecacbb25ca6e13ad0017d54805f5ec
Kieuthai1 <kieuthai093@gmail.com>
2024-04-21 00:17:00 +0700

    init project

---
+++ pages/main/danhmuc.php
@@ -0,0 +1,70 @@
+<h3>Danh mục sản phẩm: Ốp lưng</h3>
+              <ul class="product_list">
+                <li>
+                    <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>
+                                      
+                </li>
+                <li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>                   
+                </li><li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>                  
+                </li>
+                <li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>                   
+                </li>
+                <li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>                
+                </li>
+                <li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>            
+                </li>
+                <li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>            
+                </li><li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>            
+                </li><li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>            
+                </li><li>
+                <a href="">
+                        <img src="./images/tainghesony.webp" >
+                        <p class="title_product">Tên sản phẩm: Tai nghe Bluetooth chụp tai Sony WH-1000XM5</p>
+                        <p class="price_product">Giá: 6.290.000₫</p>   
+                    </a>            
+                </li>
+              </ul>
---
+++ pages/sidebar/sidebar.php
@@ -0,0 +1,9 @@
+<div class="siderbar">
+            <ul class="list_siderbar">
+                <li><a href="index.php?quanly=danhmucsanpham&id=1">Ốp lưng</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=2">Tai nghe</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=3">Cục sạc</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=1">Dán màn hình</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=1">Lót bàn phím</a></li>     
+            </ul>
+            </div>
