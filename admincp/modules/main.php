12366334dd982b8434030a6386abfd2a43dce04f
Kieuthai1 <kieuthai093@gmail.com>
2024-04-22 18:16:21 +0700

    up anh ok

--- admincp/modules/main.php
+++ admincp/modules/main.php
@@ -11,9 +11,13 @@
             if($tam=='quanlydanhmucsanpham' && $query=='them'){
                 include("modules/quanlydanhmucsp/them.php");
                 include("modules/quanlydanhmucsp/lietke.php");
-
             }elseif ($tam=='quanlydanhmucsanpham' && $query =='sua'){
                 include("modules/quanlydanhmucsp/sua.php");
+            }elseif ($tam=='quanlysp' && $query =='them'){
+                include("modules/quanlysp/them.php");
+                include("modules/quanlysp/lietke.php");
+            }elseif ($tam=='quanlysp' && $query =='sua'){
+                include("modules/quanlysp/sua.php");
             }else{
                 include("modules/dashboard.php");
             }
